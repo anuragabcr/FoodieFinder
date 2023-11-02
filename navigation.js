4ab672eebca60c94826a105916c416fa11a11763
anurag <anuragabcr@gmail.com>
2023-11-02 20:27:29 +0530

    welcome screen

--- navigation.js
+++ navigation.js
@@ -5,16 +5,19 @@ const Stack = createNativeStackNavigator();
 
 import React from "react";
 import HomeScreen from "./screens/HomeScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
 
 const navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
+        initialRouteName="Welcome"
         screenOptions={{
           headerShown: false,
         }}
       >
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
